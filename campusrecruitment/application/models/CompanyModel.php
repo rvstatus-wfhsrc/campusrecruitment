f8f2a6af82b1e7a9b6d9c5305042b75ed405e178
SSLAP045\SSLAP045 <c.krishnaragav@gmail.com>
2021-01-06 12:27:00 +0530

    @ragav send a mail for admin registered company (send the company user name and password to the inchange mail)

--- campusrecruitment/application/models/CompanyModel.php
+++ campusrecruitment/application/models/CompanyModel.php
@@ -122,6 +122,19 @@ class CompanyModel extends CI_Model {
 		}
 		$companyAddMergeData = array_merge($companyAddData, $companyData);
 		$companyAddStatus = $this->db->insert('company', $companyAddMergeData);
+		if($companyAddStatus == "1") { 
+			$message = "Dear ".$this->input->post('incharge')."<br>Congratulations..!<br>Your Details has been successfully Registered in our webSite.<br>Please update other details in Company Domain.<br>Path : ".site_url('HomeController/index')."<br>Your Login Details :<br>User Name : ".$companyUserName."<br>Password : company<br><br><br>Thank And Regards,<br>Admin<br><br>Note : Please Dont reply to this mail.";
+			$this->email->set_newline("\r\n");
+			$this->load->config('email');
+			$from = $this->config->item('smtp_user');
+			$to = $this->input->post('email');
+			$subject = "Login Details";
+			$this->email->from($from);
+			$this->email->to($to);
+			$this->email->subject($subject);
+			$this->email->message($message);
+			$this->email->send();
+		} 
 		return $companyAddStatus;
 	}
 
